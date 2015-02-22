22e2276041e5b5c398606b4ff446df841973043e
Matej Urbas <matej.urbas@gmail.com>
2015-02-22 02:57:42 +0000

    dkasjdsa

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,7 +80,7 @@ namespace :deploy do
     on roles(:app) do
       within release_path do
         with rails_env: fetch(:rails_env) do
-          execute :rake, "bower:install:deployment"
+          execute :rake, "bower:install['--force-latest']"
         end
       end
     end
