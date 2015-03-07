e1dfd141412f0380723358fd96717cd73fe5193b
Matej Urbas <matej.urbas@gmail.com>
2015-03-07 15:40:59 +0000

    Trying out structs.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,22 +1,30 @@
 module PagesHelper
 
-  include Rails.application.routes.url_helpers
+  Page = Struct.new(:name, :route_name) do
+
+    include Rails.application.routes.url_helpers
+
+    def url
+      pages_path(route_name)
+    end
+
+    def member?(route_name)
+      current_page_url = Rails.application.routes.url_for(controller: :pages, action: :page, page: route_name, only_path: true)
+      current_page_url.starts_with?(url)
+    end
+
+  end
 
   def pages
     unless defined? @pages
       @pages = [
-          {name: 'About', url: pages_path(:about)},
-          {name: 'Guide', url: pages_path(:guide)},
-          {name: 'Licence', url: pages_path(:licence)},
-          {name: 'Contact', url: pages_path(:contact)}
+          Page.new('About', :about),
+          Page.new('Guide', :guide),
+          Page.new('Licence', :licence),
+          Page.new('Contact', :contact)
       ]
     end
     @pages
-  end
-
-  def is_page_active(page)
-    current_page_url = Rails.application.routes.url_for(controller: :pages, action: :page, page: params[:page], only_path: true)
-    current_page_url.starts_with?(page[:url])
   end
 
 end
