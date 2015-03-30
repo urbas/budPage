c887a0277769eb2a161575ba4c5070df9b5a130b
Matej Urbas <matej.urbas@gmail.com>
2015-03-31 00:07:24 +0100

    Enabled submodule in capistrano.

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,6 +29,7 @@ set :ssh_options, {user: 'urbas',
 
 # Default value for :scm is :git
 # set :scm, :git
+set :git_enable_submodules, 1
 
 # Default value for :format is :pretty
 # set :format, :pretty
